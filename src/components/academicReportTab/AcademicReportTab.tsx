dacdf3b0a8a3291f8bc209142400707dfb276f47
joanromerodev <85184406+joanromerodev@users.noreply.github.com>
2025-12-14 17:36:30 -0500

    fix: remove duplicate lowercase folders

--- src/components/academicReportTab/AcademicReportTab.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoonTabs from '@/components/common/ComingSoonTabs';
-
-export default function AcademicReportTab() {
-  return <ComingSoonTabs />;
-}
--- src/components/disciplineNotesTab/DisciplineNotesTab.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoonTabs from '@/components/common/ComingSoonTabs';
-
-export default function DisciplineNotesTab() {
-  return <ComingSoonTabs />;
-}
